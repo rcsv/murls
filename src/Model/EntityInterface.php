bde73ebdc366a171ce4c4cbf1f994c9df3736a6e
rcsv <Awane.Tomohiro@gmail.com>
2023-06-04 07:50:07 +0900

    Entity, Repositoryの関係、役割分担の見直し

--- src/Model/EntityInterface.php
+++ src/Model/EntityInterface.php
@@ -1,12 +1,41 @@
 <?php
+declare(strict_types=1);
+// path: src/Model/EntityInterface.php
 
 namespace Rcsvpg\Murls\Model;
 
+// RepositoryInterface と EntityInterface は、
+// それぞれのクラスで実装する必要がある。
 interface EntityInterface extends \JsonSerializable
 {
-    //
+    /**
+     * getId
+     * get Primary Key
+     * @return int|null
+     */
     public function getId(): ?int;
-    public function setId(int $id): void;
-    public function getUniqueValue(): ?string;
-    public function getSlaveValue(): ?string;
+
+    /**
+     * getUnique
+     * User なら email, Url なら shortcode、など、それぞれのテーブルのユニークな値を返す
+     * @return string|null
+     * @throws MurlsNotSetException
+     */
+    public function getUnique(): string ;
+    //throws MurlsNotSetException
+
+    /**
+     * getSecondaryValue
+     * User なら name, Url なら longurl、など、それぞれのテーブルのユニークでない従属的な値を返す
+     * @return string|null
+     * @throws MurlsNotSetException
+     */
+    public function getSecondary(): string;
+
+    /**
+     * setSeverals
+     * @param array $data
+     * @return void
+     */
+    public function setSeverals(array $data): void;
 }
--- src/Model/RepositoryInterface.php
+++ src/Model/RepositoryInterface.php
@@ -5,17 +5,52 @@ namespace Rcsvpg\Murls\Model;
 
 interface RepositoryInterface
 {
-    function findAll() : ?array;
+    /**
+     * findAll
+     * EntityInterface[] を返す
+     * @return EntityInterface[]
+     */
+    public function findAll() : array;
 
-    function find(int $id) : ?EntityInterface;
+    /**
+     * findWhere
+     * EntityInterface[] を返す。WHERE user_id = 1 などの条件を指定する
+     * @param  string $column
+     * @param  string $value
+     * @return EntityInterface[]
+     */
+    public function findWhere(string $column, string $value) : array;
 
-    function findByTitle(string $title) : ?EntityInterface;
+    /**
+     * create
+     * Userの場合はregisterのように情報を受け取り、DBに保存、UserEntityを返す
+     * @param  EntityInterface $entity
+     * @return int
+     */
+    public function create(EntityInterface $entity): int;
 
-    function create(EntityInterface $data) : int;
+    /**
+     * read
+     * Userの場合はidを受け取り、DBから情報を取得、UserEntityを返す
+     * @param  int $id
+     * @return EntityInterface
+     */
+    public function findById(int $id): EntityInterface;
 
-    function update(EntityInterface $data) : bool;
+    /**
+     * update
+     * Userの場合はidと情報を受け取り、DBを更新、UserEntityを返す
+     * @param  EntityInterface $entity
+     * @return bool
+     */
+    public function update(EntityInterface $entity): bool;
 
-    function delete(int $id) : bool;
+    /**
+     * delete
+     * Userの場合はidを受け取り、DBから削除、boolを返す
+     * @param  int $id
+     * @return bool
+     */
+    public function delete(int $id): bool;
 
-    function count() : int;
 }
