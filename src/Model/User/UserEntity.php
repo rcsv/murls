1a1cc0521e66d34f0bed631a1fa892f8c1396ecd
rcsv <Awane.Tomohiro@gmail.com>
2023-06-04 08:36:19 +0900

    だんだんCakePHPのモデルに似てきたな...

--- src/Model/User/UserEntity.php
+++ src/Model/User/UserEntity.php
@@ -1,98 +1,64 @@
 <?php
 declare(strict_types=1);
-
+// path: src/Model/User/UserEntity.php
 namespace Rcsvpg\Murls\Model\User;
 
-use Rcsvpg\Murls\Model\EntityInterface as Entity ;
-
-class UserEntity implements Entity
-{
-    private $id ;
-    private $email ;
-    private $password ;
-    private static $password_options = [
-        'cost' => 12,
-    ];
-    private $verifyurl ;
-    private $verified ;
-    private $created ;
-    private $updated ;
-    private $deleted ;
-
-    public function __construct(array $args = []){
-        $this->id           = $args['id'] ?? 0 ;
-        $this->email        = $args['email'] ?? '' ;
-        $this->password     = $this->setPassword(args['password'] ?? '') ;
-        $this->verifyurl    = $args['verifyurl'] ?? '' ;
-        $this->verified     = (bool)$args['verified'] ;
-        $this->created      = $args['created'] ;
-        $this->updated      = $args['updated'] ;
-        $this->deleted      = $args['deleted'] ;
-    }
-
-    public function getId() : int
-    {
-        return $this->id ;
-    }
-
-    public function setId(int $id) : Entity
-    {
-        $this->id = $id ;
-        return $this ;
-    }
-
-    public function getUniqueValue() : string
-    {
-        return $this->email ;
-    }
-
-    public function getEmail() : string
-    {
-        return $this->email ;
-    }
-
-    public function setEmail(string $email) : Entity
-    {
-        $this->email = $email ;
-        return $this ;
-    }
-
-    public function checkPassword(string $password) : bool
-    {
-        return password_verify($password, $this->password) ;
-    }
-
-    public function getPassword() : string
-    {
-        return $this->password ;
-    }
-    
-    public function setPassword(string $password) : Entity
-    {
-        $this->password = password_hash(
-            $password, 
-            PASSWORD_BCRYPT, 
-            self::$password_options) ;
-        return $this ;
-    }
-
-    public function getSlaveValue() : ?string
-    {
-        return $this->getPassword() ;
-    }
-}
+use Rcsvpg\Murls\Model\AbstractEntity;
 
 /**
- * CREATE TABLE users (
- * id bigint unsigned NOT NULL AUTO_INCREMENT,
- * email varchar(255) NOT NULL,
- * password varchar(255) NOT NULL,
- * verifyurl varchar(100) default null,
- * verified tinyint(1) default 0,
- * created datetime default current_timestamp,
- * updated datetime default current_timestamp on update current_timestamp,
- * deleted datetime default null
- * PRIMARY KEY (id),
- * UNIQUE KEY (email)
- * ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+ * UserEntity
+ * 
+ * Model部分のデータベース操作を切り離したクラス
+ * Usersテーブルで扱っている情報を定義する
+ * 
+ * @package Rcsvpg\Murls\Model\User
  */
+class UserEntity extends AbstractEntity
+{
+    // user entity use a users table
+    protected $table = 'users';
+
+    protected $fillable = [
+        //'name',
+        'id',
+        'email',
+        'password',
+        'created_at',
+        'updated_at',
+        'deleted_at',
+        'verifyurl',
+        'verified',
+    ];
+
+    protected $hidden = [
+        'password',
+    ];
+
+    public function __construct(array $data = [])
+    {
+        parent::__construct($data);
+
+        // make hashing password when creating user
+        if ( isset($data['password']) ) {
+            $this->password = password_hash($data['password'], PASSWORD_DEFAULT);
+        }
+
+        // set primary and secondary field key
+        $this->primary   = 'email';
+        $this->secondary = 'email';
+    }
+
+    /**
+     * checkPassword
+     * Entityが保有しているPasswordが、
+     * 引数のPasswordと一致するかどうかをチェックする
+     * @param string $password
+     * @return bool
+     */
+    public function checkPassword(string $password): bool
+    {
+        $hashed = password_hash($password, PASSWORD_DEFAULT);
+        return $this->password !== $hashed;
+    }
+
+}
