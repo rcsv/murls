7ef0fbd46b0409c5beae8d25777151f722f828f3
rcsv <Awane.Tomohiro@gmail.com>
2023-06-03 11:10:39 +0900

    update HTML source code for registration form with adding Bootstrap CSS and JS

--- templates/auth/register.blade.php
+++ templates/auth/register.blade.php
@@ -1,53 +1,58 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="flex justify-center">
-    <form class="" method="POST">
-        <!-- email -->
-        <div class="mb-4">
-            <label class="block text-gray-700 text-sm font-bold mb-2" for="email">
-                Email
-            </label>
-            <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                   id="email" type="email" placeholder="Email" name="email" value="{{ old('email') }}">
-            @error('email')
-            <p class="text-red-500 text-xs italic">{{ $message }}</p>
-            @enderror
-        </div>
-        <!-- password -->
-        <div class="mb-4">
-            <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
-                Password
-            </label>
-            <input class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight
-                           focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************" name="password">
-            @error('password')
-            <p class="text-red-500 text-xs italic">{{ $message }}</p>
-            @enderror
-        </div>
-        <!-- password confirmation -->
-        <div class="mb-4">
-            <label class="block text-gray-700 text-sm font-bold mb-2" for="password_confirmation">
-                Password Confirmation
-            </label>
-            <input class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight
-                           focus:outline-none focus:shadow-outline" id="password_confirmation" type="password" placeholder="******************" name="password_confirmation">
-            @error('password_confirmation')
-            <p class="text-red-500 text-xs italic">{{ $message }}</p>
-            @enderror
-        </div>
-        <!-- submit -->
-        <div class="flex items-center justify-between">
-            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded
-                           focus:outline-none focus:shadow-outline" type="submit">
-                Register
-            </button>
-            <a class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="{{ route('login') }}">
-                Already have an account?
-            </a>
-        </div>
-        
 
-    </form>
+<div class="container col-xl-10 col-xxl-8 px-4 py-5">
+    <div class="row align-items-center g-lg-5 py-5">
+        <div class="col-lg-7 text-center text-lg-start">
+            <h1 class="display-4 fw-bold lh-1 mb-3">Vertically centered hero sign-up form</h1>
+            <p class="col-lg-10 fs-4">Below is an example form built entirely with Bootstrap’s form controls. Each
+                required form group has a validation state that can be triggered by attempting to submit the form
+                without completing it.</p>
+        </div>
+        <div class="col-md-10 mx-auto col-lg-5">
+            <form class="p-4 p-md-5 border rounded-3 bg-light" method="POST">
+                <div class="form-floating mb-3">
+                    <!-- email -->
+                    <input name="email" value="{{ $email }}" type="email" class="form-control" id="floatingInput"
+                        placeholder="name@example.com">
+                    <label for="floatingInput">Email address</label>
+                </div>
+                <div class="form-floating mb-3">
+                    <!-- password #1 -->
+                    <input name="password" type="password" class="form-control" id="floatingPassword-1"
+                        placeholder="Password">
+                    <label for="floatingPassword-1">Password</label>
+                </div>
+                <div class="form-floating mb-3">
+                    <!-- password #2 -->
+                    <input name="password_confirmation" type="password" class="form-control" id="floatingPassword-2"
+                        placeholder="SamePhrase">
+                    <label for="floatingPassword-2">Confirm</label>
+
+                    <div class="checkbox mb-3">
+                        <label>
+                            <input type="checkbox" value="remember-me"> Remember me
+                        </label>
+                    </div>
+                    <button class="w-100 btn btn-lg btn-primary" type="submit">Sign up</button>
+                    <script>
+                    document.querySelector('form').addEventListener('submit', function(e) {
+                        e.preventDefault();
+                        if (document.querySelector('input[name="password"]').value !== document.querySelector(
+                                'input[name="password_confirmation"]').value) {
+                            alert('Password does not match');
+                        } else {
+                            this.submit();
+                        }
+                    });
+                    </script>
+                    <hr class="my-4">
+                    <small class="text-muted">By clicking Sign up, you agree to the terms of use.</small>
+                </div>
+            </form>
+        </div>
+    </div>
 </div>
+
 @endsection
--- templates/layouts/app.blade.php
+++ templates/layouts/app.blade.php
@@ -1,13 +1,37 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="URL Shortening Service">
+    <meta name="author" content="Rcsvpg">
+
+    <!-- CSS -->
+    @include('layouts.css')
+
     <title>URL Shortening Service</title>
+    <link rel="canonical" href="https://mur.ls">
+
+    <!-- Favicons -->
+
+    <!-- Custom styles for this template -->
+    {{-- todo: link additional css --}}
+    <link href="/css/main.css" rel="stylesheet">
+
 </head>
 <body>
-<div>
-    <h1>Hello, World of blade templates!</h1>
-    @yield('content')
-</div>
+    <header>
+
+    </header>
+    <main>
+        @yield('content')
+    </main>
+    <footer>
+        
+    </footer>
+    <!-- javascript -->
+    @include('layouts.javascript')
+
 </body>
 </html>
