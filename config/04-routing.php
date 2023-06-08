0392fa954613c84c5d34a69b25c60894c7d9729c
rcsv <Awane.Tomohiro@gmail.com>
2023-06-08 20:47:43 +0900

    remove unimplemented routes yet

--- config/04-routing.php
+++ config/04-routing.php
@@ -25,33 +25,12 @@ return function (App $app) {
 
     // 3. service management mode
     $app->group('/user', function(Group $group) {
-        $group->get('/login',   [UserController::class, 'login'])->setName('login');
-        $group->post('/login',  [UserController::class, 'loginPost'])->setName('loginPost');
-        $group->get('/logout',  [UserController::class, 'logout'])->setName('logout');
-        $group->get('/signup',  [UserController::class, 'signup'])->setName('signup');
-        $group->post('/signup', [UserController::class, 'signupPost'])->setName('signupPost');
 
-        // Update and Delete
-        $group->get('/update',  [UserController::class, 'update'])->setName('update');
-        $group->post('/update', [UserController::class, 'updatePost'])->setName('updatePost');
+        // display registration form
+        $group->get('/register', [UserController::class], 'register')->setName('register');
 
-        $group->get('/delete',  [UserController::class, 'delete'])->setName('delete');
-        $group->post('/delete', [UserController::class, 'deletePost'])->setName('deletePost');
+        // process registration form
+        $group->post('/register', [UserController::class], 'registerPost')->setName('registerPost');
 
     });
-
-    $app->group('/urls', function(Group $group) {
-        $group->get('/list',    [UrlController::class, 'list'])->setName('list');
-        $group->get('/create',  [UrlController::class, 'create'])->setName('create');
-        $group->post('/create', [UrlController::class, 'createPost'])->setName('createPost');
-
-        // Update and Delete
-        $group->get('/update',  [UrlController::class, 'update'])->setName('update');
-        $group->post('/update', [UrlController::class, 'updatePost'])->setName('updatePost');
-
-        $group->get('/delete',  [UrlController::class, 'delete'])->setName('delete');
-        $group->post('/delete', [UrlController::class, 'deletePost'])->setName('deletePost');
-
-    });
-
 };
